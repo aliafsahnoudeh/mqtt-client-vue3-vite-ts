2282e699ea3be5272122f7405891112bf84320c6
Ali Afsahnoudeh <aliafsahnoudeh@gmail.com>
2023-01-08 12:33:20 +0100

    add DumpComponent

---
+++ src/components/DumpComponent.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps({
+  message: String,
+});
+</script>
+
+<template>
+  <div class="message-container">
+    {{ props.message }}
+  </div>
+</template>
